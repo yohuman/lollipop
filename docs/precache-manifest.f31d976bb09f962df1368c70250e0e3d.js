6ca50bcb5adfb5dc849143e020919f1259464923
YoHuman <hello@yohuman.io>
2021-04-13 00:48:04 +0100

    docs

---
+++ docs/precache-manifest.f31d976bb09f962df1368c70250e0e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ab3b74802ac6b6160674456eee8ccc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1725c640e268a3bea63",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "9b83bd89a4c9edc6f124",
+    "url": "./static/js/2.5a7b93a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5a7b93a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1725c640e268a3bea63",
+    "url": "./static/js/main.be0de5b2.chunk.js"
+  },
+  {
+    "revision": "bff4d32d65e83b9e0969",
+    "url": "./static/js/runtime-main.3beb0b8f.js"
+  }
+]);
