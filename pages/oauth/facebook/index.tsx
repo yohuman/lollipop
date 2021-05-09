6ce14cc2243ac80c7e5cccb28afabc9d656b4d6a
YoHuman <hello@yohuman.io>
2021-05-09 20:13:04 +0100

    no var

--- pages/oauth/facebook/index.tsx
+++ pages/oauth/facebook/index.tsx
@@ -2,25 +2,24 @@ import React, { useEffect } from "react";
 
 export default function socialLogin() {
   useEffect(() => {
-    const test = async () => {
-      var urlParams = new URLSearchParams(window.location.search);
+    const callXano = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get("code");
+
       const xanoUrl = "https://x8ki-letl-twmt.n7.xano.io/api:Ah5nAblb";
       const xanoPath = "/oauth/facebook/continue";
       const redirectUrl = "https://lollipop.yohuman.io/oauth/facebook/";
-      const code = urlParams.get("code");
 
       const options = { method: "GET" };
       const params = new URLSearchParams({ redirect_uri: redirectUrl, code });
       const response = await fetch(`${xanoUrl}${xanoPath}?${params}`, options);
       const url = await response.json();
-      console.log(url);
+      if (url) console.log(url);
 
       // close();
     };
-    test();
+    callXano();
   }, []);
-
-  // lollipop.yohuman.io/oauth/facebook?code=AQCfKTSenqouUJz18Fr42FR5qvOFIRZCB3xMUI9jiH2acjyWHucdG1DwEfKPprv7P3_6vPO9P0ap4xiLpOBIbw4NT13LZ7SaJSgNgAIkIbcNuQ_buiCEAPZYWbCEBqBn-nMEcZC0A3sXnKHOpSqkUFnwEwVRJdhSSB4DmLHSFIVzTPwELtqconpX44ohJbS5oyYJfLPwBks2iEhLKRykuLWM0xCEZPZvu6UooLS1ISiAU-Au9lEdCGW0POrcKVQxugv6nmBv1wjHtoYW3RzFIgXUsDRAApqIWzpjTOVG64T9CmnF30AHdiv2MgcHzVQNxmzc7RL7h8Vvhq6N6q4OZaqzT2GNXixWRP9LKYBjDSzlsA#_=_
 
   https: return (
     <main>
