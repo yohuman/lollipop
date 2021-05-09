23795cb9c9df3f0c48bfcc8cd3ee8e85bd6f8038
YoHuman <hello@yohuman.io>
2021-05-09 19:25:06 +0100

    get query

--- pages/oauth/facebook/index.tsx
+++ pages/oauth/facebook/index.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function socialLogin() {
-  //   useEffect(() => {
-  //     const test = () => {
-  //       close();
-  //     };
-  //     test();
-  //   }, []);
+  const router = useRouter();
 
-  return (
+  useEffect(() => {
+    const test = () => {
+      console.log(router);
+      console.log(router.query);
+      // close();
+    };
+    test();
+  }, []);
+
+  // lollipop.yohuman.io/oauth/facebook?code=AQCfKTSenqouUJz18Fr42FR5qvOFIRZCB3xMUI9jiH2acjyWHucdG1DwEfKPprv7P3_6vPO9P0ap4xiLpOBIbw4NT13LZ7SaJSgNgAIkIbcNuQ_buiCEAPZYWbCEBqBn-nMEcZC0A3sXnKHOpSqkUFnwEwVRJdhSSB4DmLHSFIVzTPwELtqconpX44ohJbS5oyYJfLPwBks2iEhLKRykuLWM0xCEZPZvu6UooLS1ISiAU-Au9lEdCGW0POrcKVQxugv6nmBv1wjHtoYW3RzFIgXUsDRAApqIWzpjTOVG64T9CmnF30AHdiv2MgcHzVQNxmzc7RL7h8Vvhq6N6q4OZaqzT2GNXixWRP9LKYBjDSzlsA#_=_
+
+  https: return (
     <main>
       <div>Redirect</div>
       <style jsx>{`
