32baf695fdfa85061366a51d3ccc8968956e347d
YoHuman <hello@yohuman.io>
2021-05-31 00:20:03 +0100

    fixed swiper

--- pages/components/swiper/index.tsx
+++ pages/components/swiper/index.tsx
@@ -1,81 +1,112 @@
-import React, { useRef, useState, useLayoutEffect, useEffect } from "react";
+import React, { useCallback, useRef, useState, useEffect } from "react";
+import Link from "next/link";
 
-export default function Swiper() {
+const Swiper = () => {
   const ref = useRef(null);
-  const [componentWidth, setComponentWidth] = useState(0);
-  const [startX, setStartX] = useState(0);
-  const [startScrollLeft, setStartScrollLeft] = useState(0);
+  const startX = useRef<number>(0);
+  const [startScrollPosition, setStartScrollPosition] = useState<number>(0);
+  const [mouseDown, setMouseDown] = useState<boolean>(false);
+  const [hideLeft, setHideLeft] = useState<boolean>(null);
+  const [hideRight, setHideRight] = useState<boolean>(null);
+  const [doClick, setDoClick] = useState<boolean>(null);
 
-  let movement_timer = null;
-
-  const test_width = () => {
-    if (ref.current) {
-      setComponentWidth(ref.current.offsetWidth);
-    }
-  };
-
-  useLayoutEffect(() => {
-    test_width();
+  const handleDown = useCallback((e: any) => {
+    setMouseDown(true);
+    if (!ref.current.contains(e.target)) return;
+    if (e.target.id === "item") setDoClick(true);
+    startX.current = e.pageX - ref.current.offsetLeft;
+    setStartScrollPosition(ref.current.scrollLeft);
   }, []);
 
-  const handleResize = () => {
-    clearInterval(movement_timer);
-    movement_timer = setTimeout(test_width, 100);
-  };
+  const handleMove = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      if (!ref.current.contains(e.target)) return;
+      setDoClick(false);
+      const mouseX = e.pageX - ref.current.offsetLeft;
+      const moveX = mouseX - startX.current;
+      if (mouseDown) {
+        ref.current.scrollLeft = startScrollPosition - moveX;
+      }
+    },
+    [mouseDown, startScrollPosition, startX]
+  );
 
-  const handleDown = (e: any) => {
-    if (!ref.current.contains(e.target)) {
-      return;
-    }
-    setStartX(e.pageX - ref.current.offsetLeft);
-    setStartScrollLeft(ref.current.scrollLeft);
-  };
-
-  const handleMove = (e: any) => {
-    e.preventDefault();
-    if (!ref.current.contains(e.target)) {
-      return;
-    }
-    const mouseX = e.pageX - ref.current.offsetLeft;
-    const moveX = mouseX - startX;
-    ref.current.scrollLeft = startScrollLeft - moveX;
+  const handleUp = (e: any) => {
+    console.log(e.target.id);
+    setMouseDown(false);
+    if (doClick) console.log("click");
+    setDoClick(false);
   };
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
+    document.addEventListener("mouseup", handleUp);
     document.addEventListener("mousedown", handleDown);
     document.addEventListener("mousemove", handleMove);
     return () => {
-      window.removeEventListener("resize", handleResize);
+      document.removeEventListener("mouseup", handleUp);
       document.removeEventListener("mousedown", handleDown);
       document.removeEventListener("mousemove", handleMove);
     };
-  }, []);
+  }, [handleDown, handleMove, handleUp]);
 
   const handleScroll = (e: any) => {
     const { scrollWidth, scrollLeft, clientWidth } = e.target;
-    if (scrollLeft + clientWidth === scrollWidth) {
-      console.log("end");
-    }
-    if (scrollLeft === 0) {
-      console.log("start");
-    }
+    setHideRight(scrollLeft + clientWidth === scrollWidth);
+    setHideLeft(scrollLeft === 0);
   };
+
+  const goLeft = () => (ref.current.scrollLeft = ref.current.scrollLeft - 400);
+  const goRight = () => (ref.current.scrollLeft = ref.current.scrollLeft + 400);
 
   return (
     <header className="container">
-      {componentWidth} - {startX} - {startScrollLeft}
+      {JSON.stringify(doClick)}
       <ul onScroll={handleScroll} ref={ref}>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>Item 5</li>
-        <li>Item 6</li>
-        <li>Item 7</li>
-        <li>Item 8</li>
+        <li id="item">Item 1</li>
+        <li id="item">Item 2</li>
+        <li id="item">Item 3</li>
+        <li id="item">Item 4</li>
+        <li id="item">Item 5</li>
+        <li id="item">Item 6</li>
+        <li id="item">Item 7</li>
+        <li id="item">Item 8</li>
       </ul>
+      {!hideLeft && (
+        <div className="left" onClick={goLeft}>
+          Left
+        </div>
+      )}
+      {!hideRight && (
+        <div className="right" onClick={goRight}>
+          Right
+        </div>
+      )}
       <style jsx>{`
+        .container {
+          position: relative;
+        }
+        .left {
+          position: absolute;
+          top: 110px;
+          width: 70px;
+          height: 70px;
+          border-radius: 50%;
+          background: white;
+          box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
+          cursor: pointer;
+        }
+        .right {
+          position: absolute;
+          top: 110px;
+          right: 0;
+          width: 70px;
+          height: 70px;
+          border-radius: 50%;
+          background: white;
+          box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
+          cursor: pointer;
+        }
         ul {
           overflow-x: auto;
           overflow-y: hidden;
@@ -84,8 +115,8 @@ export default function Swiper() {
           cursor: grab;
           padding: 0;
           display: grid;
-          grid-gap: 20px;
-          grid-template-columns: 260px 260px 260px 260px 260px 260px 260px 260px;
+          grid-gap: 10px;
+          grid-template-columns: repeat(8, 260px);
         }
         ul::-webkit-scrollbar {
           background: #ebeced;
@@ -102,10 +133,14 @@ export default function Swiper() {
           height: 230px;
           white-space: normal;
           background: grey;
+          cursor: pointer;
+          border-radius: 4px;
         }
       `}</style>
     </header>
   );
-}
+};
 
-// https://valtism.com/src/use-drag-scroll.html
+export default function App() {
+  return <Swiper />;
+}
